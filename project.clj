c36f1f15d9625231d6083839ed467b2813b1d032
Lee Hinman <lee@writequit.org>
2012-10-12 10:49:25 -0600

    bump to 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-clojuredocs "1.0.1"
+(defproject lein-clojuredocs "1.0.2-SNAPSHOT"
   :url "https://github.com/dakrone/lein-clojuredocs"
   :description "Generate data for clojuredocs"
   :eval-in :leiningen
