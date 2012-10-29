121ec1ca7ff9257316084a32e159027636ca8b9d
Lee Hinman <lee@writequit.org>
2012-10-29 00:33:48 -0600

    update to cadastre 0.1.1, which adds :source to the doc map

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :url "https://github.com/dakrone/lein-clojuredocs"
   :description "Generate data for clojuredocs"
   :eval-in :leiningen
-  :dependencies [[cadastre "0.1.0"]])
+  :dependencies [[cadastre "0.1.1"]])
--- src/leiningen/clojuredocs.clj
+++ src/leiningen/clojuredocs.clj
@@ -7,7 +7,7 @@
   [project]
   (lein/eval-in-project
    (-> project
-       (update-in [:dependencies] conj ['cadastre "0.1.0"]))
+       (update-in [:dependencies] conj ['cadastre "0.1.1"]))
    `(binding [cadastre.analyzer/*verbose* true]
       (cadastre.analyzer/gen-project-docs-json '~project))
    '(require 'cadastre.analyzer)))
